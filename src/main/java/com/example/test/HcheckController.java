af5a5c3e38ce2c9d8cdad172a4b0a39669f7af3c
wanniDev <wannidev0928@gmail.com>
2022-02-11 00:34:24 +0900

    test2

--- src/main/java/com/example/test/HcheckController.java
+++ src/main/java/com/example/test/HcheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HcheckController {
-	@GetMapping("/check")
+	@GetMapping("/hcheck")
 	public Long hcheck() {
 		return System.currentTimeMillis();
 	}
