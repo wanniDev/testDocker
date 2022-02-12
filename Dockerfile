ac1439fecd42367f52063ac47daeabe19c2d9751
wanniDev <wannidev0928@gmail.com>
2022-02-12 17:56:58 +0900

    Feat: 코드가 업데이트 되었다. GCP에 무슨일이 일어나는지 한번 확인해보자

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM openjdk:11-jdk
 VOLUME /tmp
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/test/HcheckController.java
+++ src/main/java/com/example/test/HcheckController.java
@@ -9,4 +9,9 @@ public class HcheckController {
 	public Long hcheck() {
 		return System.currentTimeMillis();
 	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
 }
